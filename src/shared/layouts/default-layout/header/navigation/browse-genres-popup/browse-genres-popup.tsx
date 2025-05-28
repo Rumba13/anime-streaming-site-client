c3c93007eb266ab6fe15aafb77d8a95d987caaf5
Senderium <senderium1@gmail.com>
2025-05-28 13:12:28 +0300

    refactor(browse-genres-popup/ANIME-1): separate quick-navigation component

--- src/shared/layouts/default-layout/header/navigation/browse-genres-popup/browse-genres-popup.tsx
+++ src/shared/layouts/default-layout/header/navigation/browse-genres-popup/browse-genres-popup.tsx
@@ -4,24 +4,14 @@ import {BrowseGenresPopupStore} from "./browse-genres-popup-store.ts";
 import clsx from "clsx/lite";
 import {observer} from "mobx-react";
 import {Genres} from "./genres/genres.tsx";
-import {ROUTES} from "../../../../../lib/routes.ts";
 import {VerticalLine} from "../../../../../ui";
+import {QuickNavigation} from "./quick-navigation/quick-navigation.tsx";
 
 export const BrowseGenresPopup = observer(() => {
     const browseGenresPopupStore = useInjection(BrowseGenresPopupStore);
 
     return <div className={clsx("browse-genres", browseGenresPopupStore.isOpened && "opened")}>
-        <div className="featured">
-            <span className="featured__title">Featured</span>
-            <div className="featured-container">
-                <a className="featured__item" href="">Browser Popular</a>
-                <a className="featured__item" href="">Browser News</a>
-                <a className="featured__item" href="">Interviews</a>
-                <a className="featured__item" href="">Guides</a>
-                <a className="featured__item" href="">Announcements</a>
-                <a className="featured__item" href={ROUTES.TRAILERS_PAGE}>Trailers</a>
-            </div>
-        </div>
+        <QuickNavigation/>
         <VerticalLine/>
         <Genres/>
     </div>
