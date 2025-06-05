82b63eb4723bd37282b862c6addaa12908955e4f
Senderium <senderium1@gmail.com>
2025-06-05 16:28:41 +0300

    feat(welcome-section): add right section part

--- src/pages/home-page/ui/home-page.tsx
+++ src/pages/home-page/ui/home-page.tsx
@@ -2,9 +2,12 @@ import "./home-page.scss";
 import {Search} from "../../../features/search";
 import {DefaultLayout} from "../../../shared/layouts";
 import {LanguageSelector} from "../../../features/language-selector";
+import {WelcomeSection} from "./welcome-section/welcome-section.tsx";
 
 export function HomePage() {
     return <DefaultLayout className="home-page" SearchSlot={Search} LanguageSelectorSlot={LanguageSelector}>
-        <div className="content"></div>
+        <div className="content">
+            <WelcomeSection/>
+        </div>
     </DefaultLayout>
 }
---
+++ src/pages/home-page/ui/welcome-section/welcome-section.tsx
@@ -0,0 +1,32 @@
+import "./welcome-section.scss";
+import WelcomeIcon from "../../../../assets/images/welcome.png"
+import WelcomeTitleIcon from "../../../../assets/images/welcome-title.png"
+
+export function WelcomeSection() {
+    return <section className="welcome-section">
+        <div className="welcome-section__left">
+
+            <br/>
+            <br/>
+            <br/>
+            <br/>
+            <br/>
+            <br/>
+            <br/>
+            <br/>
+            <br/>
+            <br/>
+
+            <br/>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad consequatur eligendi error est explicabo incidunt mollitia, non praesentium sapiente tenetur! Ab eveniet explicabo, fuga magnam qui quisquam recusandae sed voluptate!
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. A aliquid asperiores atque autem blanditiis consequatur cum eius exercitationem expedita fugit itaque iure iusto maxime molestiae non, numquam officiis quidem sed sequi similique soluta temporibus vel velit voluptatem voluptatibus? Cumque eligendi eum eveniet expedita fugit nihil, nobis porro similique. Accusamus amet, atque autem commodi dicta, dolores doloribus ipsam laboriosam laborum nisi nostrum porro quis quos, vel vero? Aliquam aliquid commodi, consequuntur cupiditate deleniti dignissimos doloribus ea eaque, esse facere id illo iure laudantium magnam maxime minima officia officiis, possimus quo recusandae repudiandae saepe tempora ullam velit veritatis. Cupiditate laborum quo repellat?
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus amet aspernatur assumenda fuga impedit nesciunt odit saepe. Adipisci aliquam architecto blanditiis cum expedita iure laboriosam minima mollitia necessitatibus nesciunt non nostrum optio placeat quibusdam quod sequi similique soluta suscipit, ullam voluptate? Dolorem ducimus exercitationem iure quidem ratione. Dolores eos iste iure quibusdam sed? Accusamus, deleniti doloremque dolorum eum facere labore laboriosam nemo nihil optio quis quisquam quo reiciendis sapiente. Amet asperiores at ea, exercitationem expedita facere numquam odio perferendis porro possimus quidem reiciendis repellendus vel voluptates, voluptatibus? Libero, quae, vel. Ad alias asperiores aspernatur blanditiis commodi corporis cum dolorem, eligendi est eum eveniet facere ipsa laboriosam nesciunt non nostrum porro quas quasi repellat rerum, saepe unde vero. Ab atque beatae cumque delectus, doloribus eaque earum eligendi esse exercitationem illo laudantium magnam mollitia nam nihil nostrum officia quasi quo recusandae rerum similique ullam veritatis voluptate! Dolores esse eum harum itaque sequi? Aliquam, architecto at, autem eius eligendi error est, exercitationem illum laboriosam laborum minima quisquam sed soluta unde vitae. Aliquid atque delectus deserunt eum illo labore laborum mollitia nemo, nostrum perspiciatis repellendus rerum saepe sit ut veritatis! Accusamus culpa dolorem quaerat! Adipisci fugit laboriosam maiores, numquam praesentium quo saepe tenetur velit?
+        </div>
+        <div className="welcome-section__right">
+
+            <img className="welcome-section__welcome-icon" src={WelcomeIcon} alt=""/>
+
+            <img className="welcome-section__welcome-title-icon" src={WelcomeTitleIcon} alt=""/>
+        </div>
+    </section>
+}
