8e949db5011e20edecd5d9914370f6b530a0c841
Senderium <senderium1@gmail.com>
2025-06-05 14:01:38 +0300

    chore(i18n): update i18n-browser-resolve-language from 3.0.0 to 8.1.0
    Also update config in src/app/i18n/i18n.ts to match new requirements

--- src/app/i18n/i18n.ts
+++ src/app/i18n/i18n.ts
@@ -3,6 +3,7 @@ import {initReactI18next} from "react-i18next";
 import LanguageDetector from "i18next-browser-languagedetector";
 import ruTranslation from "./translation/ru/common.json"
 import enTranslation from "./translation/en/common.json"
+import XHR from "i18next-http-backend"
 
 export const defaultNS = "common";
 
@@ -16,18 +17,22 @@ export const resources = {
 } as const;
 
 void i18n
+    .use(XHR)
     .use(initReactI18next)
     .use(LanguageDetector)
     .init({
         lng: "en",
+        supportedLngs: ['ru', 'en'],
         ns: ["common"],
-        detection: {order: ["path", "navigator"]},
+        detection: {
+            order: ["localStorage", "cookie", "navigator", "htmlTag"],
+        },
         fallbackLng: "en",
         resources,
         defaultNS,
         interpolation: {
             escapeValue: false
-        }
+        },
     });
 
 export default i18n;
