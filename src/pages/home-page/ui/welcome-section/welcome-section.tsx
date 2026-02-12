fbde1f915b8f065b00a563b76ee81440243bddb7
Senderium <senderium1@gmail.com>
2026-02-12 17:46:01 +0300

    refactor(welcome-section-variants): delete other welcome section variants

--- src/pages/home-page/ui/welcome-section/welcome-section.tsx
+++ src/pages/home-page/ui/welcome-section/welcome-section.tsx
@@ -3,7 +3,6 @@ import TriangleIcon from "../../../../assets/images/triangle.svg?react";
 import DiagonalArrowIcon from "../../../../assets/images/diagonal-arrow.svg?react";
 import {ReviewPreview} from "./review-preview/review-preview";
 import {useTranslation} from "react-i18next";
-import {welcomeSectionVariants} from "./welcome-section-variants";
 import {
     buttonRedStyle,
     buttonsStyle,
@@ -22,28 +21,31 @@ import {
 import {observer} from "mobx-react";
 import {WelcomeBackground} from "./welcome-background/welcome-background";
 import {ROUTES} from "../../../../shared/lib";
+import WelcomeIcon1 from "../../../../assets/images/welcome-1.png";
+import WelcomeTitleIcon1 from "../../../../assets/images/welcome-title-1.png";
+import {t} from "i18next";
 
 export const WelcomeSection = observer(() => {
-    const {t:tCommon} = useTranslation("common");
-    const {title, subTitle, welcomeImage, welcomeTitleImage} = welcomeSectionVariants[0];
+    const {t: tCommon} = useTranslation("common");
 
     return (
         <section css={sectionStyle}>
             <WelcomeBackground/>
 
             <div css={leftSectionStyle}>
-                <span css={titleStyle}>{title}<span css={highlightStyle}> EpicAnime</span></span>
-                <span css={subTitleStyle}>{subTitle}</span>
+                <span css={titleStyle}>{t("welcome_to")}<span css={highlightStyle}> EpicAnime</span></span>
+                <span css={subTitleStyle}>{t("embark_on_a_journey_of_endless_stories_and_epic_adventures")}</span>
                 <div css={buttonsStyle}>
-                    <NavigateButton styles={buttonStyle} title={tCommon("explore_now")} href={ROUTES.SEARCH_PAGE} icon={<DiagonalArrowIcon/>}/>
+                    <NavigateButton styles={buttonStyle} title={tCommon("explore_now")} href={ROUTES.SEARCH_PAGE}
+                                    icon={<DiagonalArrowIcon/>}/>
                     <NavigateButton styles={[buttonStyle, buttonRedStyle]} title={tCommon("start_watching")} href="#"
                                     icon={<TriangleIcon/>}/>
                 </div>
                 <ReviewPreview styles={reviewPreviewStyle}/>
             </div>
             <div css={rightSectionStyle}>
-                <div css={iconWrapperStyle}><img css={iconStyle} src={welcomeImage} alt=""/></div>
-                <img css={titleIconStyle} src={welcomeTitleImage} alt=""/>
+                <div css={iconWrapperStyle}><img css={iconStyle} src={WelcomeIcon1} alt=""/></div>
+                <img css={titleIconStyle} src={WelcomeTitleIcon1} alt=""/>
             </div>
         </section>
     );
--- src/shared/lib/genres-to-select-options.tsx
+++ src/shared/lib/genres-to-select-options.tsx
@@ -1,6 +1,7 @@
 import {Genre} from "../types";
 import {SelectProps} from "antd";
 import {genreTypeToTranslationKey} from "./i18n/genre-type-to-translation-key.ts";
+import i18n from "i18next";
 
 export const genresToSelectOptions = (genres: Genre[]): SelectProps["options"] =>
-    genres.map(({mal_id, name}) => ({label: genreTypeToTranslationKey(name), value: mal_id}));
+    genres.map(({mal_id, name}) => ({label: i18n.t(genreTypeToTranslationKey(name), {ns: "genres"}), value: mal_id}));
